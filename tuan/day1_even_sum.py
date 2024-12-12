c6c9b1acc9b81785445e9ea835a47b9188747b10
TuanK5 <tuanvvq1708@gmail.com>
2024-12-13 00:02:13 +0700

    [tuan][day1] even sum practice.

---
+++ tuan/day1_even_sum.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+total = 0
+for i in range(1, 101):  # Duyệt qua từ 1 đến 100
+    if i % 2 == 0:       # Kiểm tra nếu là số chẵn
+        total += i
+print("Tổng các số chẵn từ 1 đến 100 là:", total)
