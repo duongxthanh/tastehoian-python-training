ab6670995dc188aa0a085cdbc9d629c9a90ffdee
hoangtienvnn95 <hoangtienvnn95@gmail.com>
2024-12-13 22:38:34 +0700

    code for tien

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-total = 0
-for i in range(1, 101):  # Duyệt qua từ 1 đến 100
-    if i % 2 == 0:       # Kiểm tra nếu là số chẵn
-        total += i
+total = sum(range(2, 101, 2))  # Tính tổng dãy số chẵn từ 2 đến 100
 print("Tổng các số chẵn từ 1 đến 100 là:", total)
