7d331b17ddc6117138cfee21c5baeacc83b46db4
NguyenTien01 <nguyentien25082001@gmail.com>
2024-12-21 14:59:15 +0700

    [nguyentien][day2]seleniumtiki.py

--- nguyentien/day2_seleniumwebsite.py
+++ nguyentien/day2_seleniumwebsite.py
@@ -7,7 +7,7 @@ import time
 # Khởi động trình duyệt
 driver = webdriver.Chrome()  # Đảm bảo bạn đã cài đặt ChromeDriver
 driver.get("https://tiki.vn/binh-giu-nhiet-locknlock-metro-drive-lhc4277s-650ml-kem-ong-hut-va-co-rua-p180378151.html?spid=180378153")
-driver.set_window_size(1920, 1080)
+# driver.set_window_size(1920, 1080)
 
 # Chờ trang tải
 wait = WebDriverWait(driver, 10)
@@ -24,15 +24,14 @@ try:
 
     # Tìm nút "Xem thêm" nếu có
     try:
-        show_more_button = driver.find_elements(By.XPATH, "//span[@class='show-more-content']")
+        show_more_button = driver.find_elements(By.CSS_SELECTOR, " span .show-more-content")
         show_more_button.click()  # Nhấp vào nút "Xem thêm"
         time.sleep(2)  # Chờ 2 giây để tải bình luận
     except Exception as e:
         print("Không tìm thấy nút 'Xem thêm' hoặc không cần nhấn")
 
     # Lấy tất cả bình luận
-    comments = driver.find_elements(By.XPATH,
-                                    "//div[@class='review-comment']/div[@class='review-comment__content']/div/span")
+    comments = driver.find_elements(By.CSS_SELECTOR, ".review-comment__content ")
     comment_list = [comment.text for comment in comments]
 
     print(f"Tìm thấy {len(comments)} bình luận")
